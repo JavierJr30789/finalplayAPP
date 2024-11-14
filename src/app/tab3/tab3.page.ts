1023c28046c503d501f6cdc5d525a1eef7e67b57
javierjr30789 <javierjr30789@gmail.com>
2024-11-14 20:25:12 -0300

    Firebase implementado y primera parte del login hecha, ya envia datos a firebase y devuelve una respuesta del mismo

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,15 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonicModule, IonModal } from '@ionic/angular';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { AuthService } from '../auth.service';
 
-import { IonicModule } from '@ionic/angular';
-
-
-@Component({
-  selector: 'app-tab3',
-  templateUrl: 'tab3.page.html',
+@Component({ 
+  selector: 'app-tab3', 
+  templateUrl: 'tab3.page.html', 
   styleUrls: ['tab3.page.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule, ReactiveFormsModule],
 })
-export class Tab3Page {
-  constructor() {}
+export class Tab3Page implements OnInit {
+  @ViewChild('modal', { static: false }) modal!: IonModal;
+  formsReg: FormGroup;
+
+  constructor(private authService: AuthService) {
+    this.formsReg = new FormGroup({
+      email: new FormControl(),
+      password: new FormControl(),
+    });
+  }
+
+  ngOnInit(): void {}
+
+  onSubmit() {
+    this.authService.register(this.formsReg.value)
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => console.log(error));
+    console.log(this.formsReg.value);
+  }
+
+  // Método que se llama cuando el modal está por mostrarse
+  onWillPresent() {
+    document.getElementById('content-background')?.setAttribute('inert', '');
+  }
+
+  // Método que se llama cuando el modal está por cerrarse
+  onWillDismiss() {
+    document.getElementById('content-background')?.removeAttribute('inert');
+  }
 }
