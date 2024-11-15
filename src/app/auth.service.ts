bc2469b7c67792c6ff873ce5e18c940e463ba967
javierjr30789 <javierjr30789@gmail.com>
2024-11-14 21:08:49 -0300

    funcion para validar credenciales (login) creada y comentada

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Auth, createUserWithEmailAndPassword } from '@angular/fire/auth';
+import { Auth, createUserWithEmailAndPassword,signInWithEmailAndPassword } from '@angular/fire/auth';
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +11,10 @@ export class AuthService {
   register({email, password}: any){
     return createUserWithEmailAndPassword(this.auth, email, password)
   }
+
+Login({email, password}: any){
+  return signInWithEmailAndPassword(this.auth, email, password);
+}
 
 
 }
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { IonicModule, IonModal } from '@ionic/angular';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { AuthService } from '../auth.service';
+import {Router } from '@angular/router';
+import { logIn } from 'ionicons/icons';
 
 @Component({ 
   selector: 'app-tab3', 
@@ -13,9 +15,18 @@ import { AuthService } from '../auth.service';
 export class Tab3Page implements OnInit {
   @ViewChild('modal', { static: false }) modal!: IonModal;
   formsReg: FormGroup;
+  formLog: FormGroup;
 
-  constructor(private authService: AuthService) {
+  constructor(
+    private authService: AuthService,
+    private router : Router
+  ) {
     this.formsReg = new FormGroup({
+      email: new FormControl(),
+      password: new FormControl(),
+    });
+
+    this.formLog = new FormGroup({
       email: new FormControl(),
       password: new FormControl(),
     });
@@ -23,13 +34,34 @@ export class Tab3Page implements OnInit {
 
   ngOnInit(): void {}
 
-  onSubmit() {
+  //funcion para crear un cuenta en la base de datos de firebase 
+  Registro() {
+    //llamamos a la funcion de register del servicio auth
     this.authService.register(this.formsReg.value)
+    //si sale bien crea el usuario nuevo en la BD
       .then(response => {
+         this.router.navigate(['/tabs/tab1'])
         console.log(response);
       })
+      // si sale mal es que enviaste datos invalidos (por ejemplo una contraseña de menos de 6 caracteres)
       .catch(error => console.log(error));
     console.log(this.formsReg.value);
+
+  }
+
+  //funcion para validar que las credenciales ingresadas son correctas o que existen en la base de datos
+  Logeo(){
+    //llamamos a la funcion login del servicio auth
+this.authService.Login(this.formLog.value)
+//si sale bien devuelve la respuesta de la BD de que te logeaste con exito
+.then(response =>{
+  
+  console.log(response);
+
+})
+//si sale mal devuelve la respuesta de la BD de que las credenciales no son validas
+.catch(error => console.log(error));
+console.log(this.formsReg.value);
   }
 
   // Método que se llama cuando el modal está por mostrarse
